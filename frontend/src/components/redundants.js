db9f638ed3aa855bbf08f57668908029ceaaf9b5
ealpha072 <ealpha072@gmail.com>
2023-02-26 16:13:08 +0300

    About us page done

--- frontend/src/components/redundants.js
+++ frontend/src/components/redundants.js
@@ -80,4 +80,31 @@
                         Our team of experts is dedicated to providing you with tailor-made solutions 
                         that meet your unique needs. We understand that every business is different, 
                         and we take the time to understand your business goals and aspirations. 
-                    </p>*/
+                    </p>
+
+<p className="">Our Story</p>
+					<p>
+					<strong>[Maitri Capital]</strong> was founded in 2016 by two
+					partners who shared a passion for investing in Africa. At the
+					time, the African investment landscape was full of challenges and
+					opportunities, and the founders saw an opportunity to create a
+					firm that could help investors navigate these challenges and
+					capitalize on the opportunities. Today, [Company Name] has grown
+					into a leading provider of investment and corporate advisory
+					services in Africa. We have a team of experts who are dedicated to
+					providing our clients with the highest level of service and
+					support, and we have established long-standing relationships with
+					our clients and partners across the region.
+					</p>
+					<p>
+					The firm's focus on research and local expertise helped it stand
+					out in a crowded market, and it quickly became known as a go-to
+					firm for investors looking to enter or expand in Africa.
+					</p>
+					<p>
+					Despite our growth, we remain committed to our founding principles
+					of local expertise, research capabilities, commitment to
+					excellence, and long-standing relationships. We believe that these
+					principles are the key to our success, and we will continue to
+					uphold them as we grow and expand in the years to come.
+					</p>*/
